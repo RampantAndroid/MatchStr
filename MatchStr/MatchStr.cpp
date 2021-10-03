fa5b7445e51ca464c571f145194845a43c09172d
RampantAndroid <82689782+RampantAndroid@users.noreply.github.com>
2021-10-03 16:56:05 -0700

    Changes

--- MatchStr/MatchStr.cpp
+++ MatchStr/MatchStr.cpp
@@ -2,40 +2,8 @@
 
 using namespace std;
 
-bool MatchStr(string haystack, int& iHaystack, string needle, int& iNeedle)
-{
-    for (int i = iHaystack; i < haystack.length(); i++)
-    {
-        if (haystack[i] == needle[iNeedle])
-        {
-            int j = iNeedle;
-            int k = 0;
-            while (i + k < haystack.length() && j < needle.length() && haystack[i + k] == needle[j])
-            {
-                k++;
-                j++;
-            }
-
-            if (needle[j] == '*')
-            {
-                iHaystack = i + k;
-                iNeedle = j + 1;
-                return false;
-            }
-
-            if (j == needle.length())
-                return true;
-        }
-    }
-
-    return false;
-}
-
-
 // Assumptions:
 // It is assumed needle begins and ends with a wildcard
-// That the needle string does not begin or end with an explicit wildcard
-// A*Z is valid. A* or *Z is invalid.
 // Method is case sensitive
 // Haystack containing a wildcard is not supported
 bool MatchStr(string haystack, string needle)
@@ -46,29 +14,70 @@ bool MatchStr(string haystack, string needle)
     // Early exits:
     // Either string is zero length
     // Needle is longer than haystack
-    // Needle begins or ends with *
     if (haystack.length() == 0 || needle.length() == 0 || 
-        haystack.length() < needle.length() ||
-        needle[0] == '*' || needle[needle.length()-1] == '*')
+        haystack.length() < needle.length())
     {
         return false;
     }
 
-    bool retval = MatchStr(haystack, iHaystack, needle, iNeedle);
-
-    // If false and the needle counter is nonzero, then a wildcard was hit.
-    // Call a second time with offsets and return result.
-    if (retval == false && iNeedle > 0)
+    if (needle.length() == 1 && needle[0] == '*')
     {
-        retval = MatchStr(haystack, iHaystack, needle, iNeedle);
+        return true;
     }
 
-    return retval;
+    if (needle[iNeedle] == '*')
+        iNeedle++;
+
+    bool singleChar = false;
+
+    while(iHaystack < haystack.length())
+    {
+        if (haystack[iHaystack] == needle[iNeedle])
+        {
+            int j = iNeedle;
+            int k = 0;
+            while (iHaystack + k < haystack.length() && j < needle.length() && haystack[iHaystack + k] == needle[j])
+            {
+                k++;
+                j++;
+            }
+
+            if (needle[j] == '*')
+            {
+                iNeedle = j+1;
+                iHaystack += k;
+            }
+            else if (needle[j] == '?')
+            {
+                iNeedle = j + 1;
+                iHaystack += k;
+                singleChar = true;
+            }
+
+            if (iNeedle == needle.length())
+                return true;
+        }
+        else if (singleChar == true)
+        {
+            return false;
+        }
+
+        iHaystack++;
+    }
+
+    return false;
+
 }
 
+// Open question:
+// Haystack = "Hello World"
+// Needle = "W*d?"
+// Does the '?' at the end, which can match nothing other than the null terminator, get a pass?
+// Or does ending the needle with '?' require there to be one final character in the haystack?
+// Similarly, what if the needle begins with '?', eg: "?Hello World"
 int main()
 {
-    bool result = MatchStr("Hello world", "w*ds");
+    bool result = MatchStr("Hello world", "*");
 
     cout << "The result was " << result;
 }
